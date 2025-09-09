9f8bc3f151f87d4dcd486e66348de3e72be2d41b
Kyosuke1004 <kyosukeippoku297@icloud.com>
2025-09-09 12:30:58 +0900

    Books CRUD機能を完成
    - 新規作成：著者の選択・新規追加機能付き
    - 詳細表示：本の情報と著者、貸出状況を表示
    - 編集機能：既存データの更新と著者変更
    - 削除機能：詳細ページから安全に削除
    - 一覧表示：著者情報も含めた表示に改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,67 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
 
   def index
     @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+    @book.authors.build
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if params[:book][:new_author_name].present?
+      author = Author.find_or_create_by(name: params[:book][:new_author_name])
+      @book.authors << author
+    end
+
+    if @book.save
+      redirect_to @book, notice: '本が正常に作成されました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if params[:book][:new_author_name].present?
+      author = Author.find_or_create_by(name: params[:book][:new_author_name])
+
+      # 既存の著者IDリストに新しい著者のIDを追加
+      author_ids = Array(params[:book][:author_ids]).reject(&:blank?)
+      author_ids << author.id.to_s unless author_ids.include?(author.id.to_s)
+
+      # paramsを更新
+      params[:book][:author_ids] = author_ids
+    end
+
+    if @book.update(book_params)
+      redirect_to @book, notice: '本が正常に更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_url, notice: '本が正常に削除されました。'
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :isbn, :published_year, :publisher, author_ids: [])
   end
 end
