f93d41dcade4306a29b52ef86e3b0f5e82fd6e01
Kyosuke1004 <kyosukeippoku297@icloud.com>
2025-09-14 21:20:09 +0900

    BooksControllerのリファクタリング: ABC Sizeを削減
    - createとupdateメソッドの著者設定ロジックをassign_authors_to_bookに抽出
    - createとupdateのロジックを統一（assign_attributes + save）
    - compact_blankを使用してコードを簡潔化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,13 +27,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-
-    ids = Array(params[:book][:author_ids]).reject(&:blank?)
-    if (name = params[:book][:new_author_name]).present?
-      author = Author.find_or_create_by!(name: name)
-      ids << author.id unless ids.include?(author.id)
-    end
-    @book.author_ids = ids
+    assign_authors_to_book
 
     if @book.save
       redirect_to @book, notice: '本が正常に作成されました。'
@@ -43,15 +37,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    ids = Array(params[:book][:author_ids]).reject(&:blank?)
-    if (name = params[:book][:new_author_name]).present?
-      author = Author.find_or_create_by!(name: name)
-      # createと同じロジックに統一
-      ids << author.id unless ids.include?(author.id)
-    end
-    @book.author_ids = ids
+    @book.assign_attributes(book_params)
+    assign_authors_to_book
 
-    if @book.update(book_params)
+    if @book.save
       redirect_to @book, notice: '本の情報が正常に更新されました。'
     else
       render :edit, status: :unprocessable_content
@@ -71,5 +60,14 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :isbn, :published_year, :publisher, author_ids: [])
+  end
+
+  def assign_authors_to_book
+    ids = Array(params[:book][:author_ids]).compact_blank
+    if (name = params[:book][:new_author_name]).present?
+      author = Author.find_or_create_by!(name: name)
+      ids << author.id unless ids.include?(author.id)
+    end
+    @book.author_ids = ids
   end
 end
