a8ce7b3350540e6e328a84c24a61ca07666695cf
Kyosuke1004 <kyosukeippoku297@icloud.com>
2025-09-30 15:38:55 +0900

    application.jsのインポート文をimportmap名に修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // application.js
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./author_autocomplete"
-import { setupNavbar } from "./navbar"
-import { setupAuthorInputValidation } from "./book_form_validation"
+import { setupNavbar } from "navbar"
+import { setupAuthorInputValidation } from "book_form_validation"
+import "author_autocomplete"
 
 // Turbo対応: ページロードごとに必ず実行される
 document.addEventListener('turbo:load', () => {
